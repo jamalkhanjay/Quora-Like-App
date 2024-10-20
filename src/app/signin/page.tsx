294d64db2bb1b1e73398abfa990e0d735f8796af
jamalkhanjay <jamalkhanwaxir@gmail.com>
2024-10-21 01:16:01 +0500

    Singup | Signin Buttons disability functionality added

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -97,7 +97,8 @@ const SignIn = () => {
             <button
               // type="submit"
               onClick={handleSignIn}
-              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              className="flex w-full justify-center rounded-md bg-indigo-600 disabled:bg-indigo-400 disabled:cursor-not-allowed px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              disabled={!email || !password}
             >
               Sign in
               {/* <Toaster toastOptions={{ duration: 5000 }} /> */}
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -124,8 +124,9 @@ const SignUp = () => {
         <div>
           <button
             // type="submit"
-            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            className="flex w-full justify-center rounded-md bg-indigo-600 disabled:cursor-not-allowed disabled:bg-indigo-400 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             onClick={createAccount}
+            disabled={!userName || !email || !password}
           >
             Sign Up
             {/* <Toaster toastOptions={{ duration: 5000 }} /> */}
