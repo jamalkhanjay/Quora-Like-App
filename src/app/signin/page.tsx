747cbf7e69bba6e7770634951003b5d5f7dc59ee
jamalkhanjay <jamalkhanwaxir@gmail.com>
2024-10-24 13:07:04 +0500

    Minor change in signin | Signup

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -100,7 +100,7 @@ const SignIn = () => {
             <button
               // type="submit"
               onClick={handleSignIn}
-              className="flex w-full justify-center rounded-md bg-red-800 disabled:bg-red-500 disabled:cursor-not-allowed px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-red-900 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
+              className="flex w-full justify-center rounded-md bg-red-800 disabled:bg-gray-300 disabled:cursor-not-allowed px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-red-900 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
               disabled={!email || !password}
             >
               Sign in
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -124,7 +124,7 @@ const SignUp = () => {
           <div>
             <button
               // type="submit"
-              className="flex w-full justify-center rounded-md bg-red-600 disabled:cursor-not-allowed disabled:bg-red-400 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
+              className="flex w-full justify-center rounded-md bg-red-600 disabled:cursor-not-allowed disabled:bg-gray-300 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
               onClick={createAccount}
               disabled={!userName || !email || !password}
             >
